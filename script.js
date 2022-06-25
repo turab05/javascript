9041e77e0e52b9fdcf1ac985a5f3de4db817d803
unknown <sttsvideo@gmail.com>
2022-06-24 23:03:07 -0700

    javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+// var a =10;
+// var b=3;
+// var c=1;
+// if (c>a) {
+//     console.log("true");
+// }else if(c>b){
+//     console.log("true1");
+// }else{
+//     console.log("false");
+// }
+// console.log(a--);
+// console.log(a);
+
+// var age=22;
+// age>18?console.log("vote"):console.log("not");
+
+var x=6;
+var y=3;
+console.log(x>5 && y>1);
+
+// console.log(x>5 || y<1);
+
+// console.log(!(x==y));
+
